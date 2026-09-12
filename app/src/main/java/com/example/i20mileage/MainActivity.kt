1469cdeafdee9cdde4f51fccbcbb31cc95d322ae
soujanesmith-rgb <soujane.smith@gmail.com>
2026-09-12 11:15:46 +0530

    UI v1 fixed 001

--- app/src/main/java/com/example/i20mileage/MainActivity.kt
+++ app/src/main/java/com/example/i20mileage/MainActivity.kt
@@ -31,6 +31,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.*
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
